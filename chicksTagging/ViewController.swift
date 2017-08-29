1cb2308e88866974114c3d023ac7b7f9cd846dfe
nightcodelearning <nightcodelearning@gmail.com>
2017-08-28 22:39:06 -0500

    Initial Commit

---
+++ chicksTagging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chicksTagging
+//
+//  Created by Inés Rojas on 8/28/17.
+//  Copyright © 2017 Inés Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
